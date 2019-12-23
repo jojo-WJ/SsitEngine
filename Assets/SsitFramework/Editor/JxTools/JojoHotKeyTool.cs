ce2b051d3d49eac7e547e2303225c06b5bb77b0f
jojo <1059114801@qq.com>
2019-12-23 17:13:57 +0800

    主干提交

---
+++ Assets/SsitFramework/Editor/JxTools/JojoHotKeyTool.cs
@@ -0,0 +1,97 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace SsitEngine.Editor
+{
+    public class JojoHotKeyTool : ScriptableObject
+    {
+        //  设置菜单Tool 下的 MyTool 下的 Enable\Disable Multi GameObj 快捷键为  command 加shift 加 d  <MAC上的>
+        public const string
+            MENU_DISABLE_SELECTED_GAMEOBJ =
+                "Tools/MyTool/Enable\\Disable Multi GameObj %#z"; //%#d 即代表 command 加shift 加 d快捷键
+
+        [MenuItem(MENU_DISABLE_SELECTED_GAMEOBJ, true)]
+        static bool ValidateSelectEnableODisable()
+        {
+            GameObject[] gobj = GetSelectedGameObject() as GameObject[];
+            if (gobj == null)
+            {
+                return false;
+            }
+            if (gobj.Length == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        [MenuItem(MENU_DISABLE_SELECTED_GAMEOBJ)]
+        static void SelectEnableODisable()
+        {
+            GameObject[] gobj = GetSelectedGameObject() as GameObject[];
+            bool enable = !gobj[0].activeSelf;
+            foreach (GameObject go in gobj)
+            {
+                EnableODisableChildNote(go.transform, enable);
+            }
+        }
+
+        //激活或者关闭选中的物体及其子物体
+        public static void EnableODisableChildNote( Transform parent, bool enable )
+        {
+            parent.gameObject.SetActive(enable);
+            /* for (int i = 0; i < parent.childCount; i++)
+             {
+                 Transform child = parent.GetChild(i);
+                 if (child.childCount != 0)
+                 {
+                     EnableODisableChildNote(child, enable);
+                 }
+                 else
+                 {
+                     child.gameObject.active = enable;
+                 }
+             }*/
+        }
+
+        // 返回选中的物体
+        static GameObject[] GetSelectedGameObject()
+        {
+            return Selection.gameObjects;
+        }
+
+        [UnityEditor.MenuItem("Tools/MyTool/Apply Selected Prefabs")]
+        static void ApplySelectedPrefabs()
+        {
+            //获取选中的gameobject对象
+            GameObject[] selectedsGameobject = Selection.gameObjects;
+
+            //GameObject prefab = PrefabUtility.FindPrefabRoot(selectedsGameobject[0]);
+
+            for (int i = 0; i < selectedsGameobject.Length; i++)
+            {
+                GameObject obj = selectedsGameobject[i];
+
+                //UnityEngine.Object newsPref = PrefabUtility.GetPrefabObject(obj);
+                UnityEngine.Object newsPref = PrefabUtility.GetPrefabInstanceHandle(obj);
+
+                //判断选择的物体，是否为预设
+                if (PrefabUtility.GetPrefabInstanceStatus(obj) == PrefabInstanceStatus.Connected)
+                {
+                    //UnityEngine.Object parentObject = PrefabUtility.GetPrefabParent(obj);
+                    UnityEngine.Object parentObject = PrefabUtility.GetCorrespondingObjectFromSource(obj);
+
+                    //获取路径
+                    string path = AssetDatabase.GetAssetPath(parentObject);
+                    //Debug.Log("path:"+path);
+                    //替换预设
+                    //PrefabUtility.ReplacePrefab(obj, parentObject, ReplacePrefabOptions.ConnectToPrefab);
+                    PrefabUtility.SaveAsPrefabAssetAndConnect(obj, path, InteractionMode.UserAction);
+
+                    //刷新
+                    AssetDatabase.Refresh();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/SsitFramework/Editor/SsitEngineInspector/SimpleGameWelcomScreen.cs
@@ -0,0 +1,74 @@
+﻿using SsitEngine.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace SimpleGameEditor
+{
+    public class SimpleGameWelcomScreen : EditorWindow
+    {
+
+        private static readonly string URL = "https://github.com/jojo-WJ/SsitEngine.git";
+        private static readonly Vector2 PreSize = new Vector2(340f, 410f);
+
+        private Rect mWelcomeScreenImageRect = new Rect(0.0f, 0.0f, 340f, 44f);
+        private Rect mWelcomeIntroRect = new Rect(70f, 57f, 250f, 20f);
+
+        private Rect mDocImageRect = new Rect(15f, 124f, 53f, 50f);
+        private Rect mDocHeaderRect = new Rect(70f, 123f, 250f, 20f);
+        private Rect mDocDescriptionRect = new Rect(70f, 143f, 250f, 30f);
+
+        private Rect mUserHeaderRect = new Rect(70f, 165f, 250f, 20f);
+        private Rect mUserInputRect = new Rect(70f, 185f, 250f, 20f);
+
+        private Rect mToggleButtonRect = new Rect(220f, 385f, 125f, 20f);
+        private Texture mWelcomeScreenImage;
+        
+        [MenuItem("Tools/SsitEngine/Welcome Screen", false, 3)]
+        public static void ShowWindow()
+        {
+            SimpleGameWelcomScreen window = GetWindow<SimpleGameWelcomScreen>(true, "Welcome to SsitEngine");
+            window.maxSize = PreSize;
+            window.minSize = PreSize;
+        }
+        public void OnEnable()
+        {
+            GameReference.InitPrefernces();
+        }
+        
+        public void OnGUI()
+        {
+            //GUI.DrawTexture(this.mWelcomeScreenImageRect, this.mWelcomeScreenImage);
+            GUI.Label(this.mWelcomeIntroRect, "Welcome To SsitEngine", GUIUtils.TextHeaderGUIStyle);
+
+            GUI.Label(this.mDocHeaderRect, "Documentation", GUIUtils.TextHeaderGUIStyle);
+            GUI.Label(this.mDocDescriptionRect, URL, GUIUtils.TextDescriptionGUIStyle);
+            
+            GUI.Label(this.mUserHeaderRect, "User", GUIUtils.TextHeaderGUIStyle);
+            var  userName = GUI.TextField(this.mUserInputRect, GameReference.GetString(BDPreferences.UserName));
+
+            if (GUI.changed)
+            {
+                GameReference.SetString(BDPreferences.UserName,userName);
+            }
+            
+            bool flag = GUI.Toggle(this.mToggleButtonRect, GameReference.GetBool(BDPreferences.ShowWelcomeScreen), "Show at Startup");
+            if (flag != GameReference.GetBool(BDPreferences.ShowWelcomeScreen))
+            {
+                GameReference.SetBool(BDPreferences.ShowWelcomeScreen, flag);
+            }
+            EditorGUIUtility.AddCursorRect(this.mDocDescriptionRect, MouseCursor.Link);
+            if (Event.current.type != EventType.MouseUp)
+                return;
+            Vector2 mousePosition = Event.current.mousePosition;
+            if (mDocDescriptionRect.Contains(mousePosition))
+            {
+                Application.OpenURL(URL);
+            }
+        }
+
+        /*public void OnDisable()
+        {
+            
+        }*/
+    }
+}
